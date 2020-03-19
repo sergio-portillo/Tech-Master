2a6350e6f1e6f8602ba32e991ecf608a51649dfd
sergio <sergioportillo2013@outlook.com>
2020-03-19 15:18:46 -0700

    Archivos Originales

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var elementTop = $('.nav').offset().top;
+
+$(window).scroll(function(){
+    if($(window).scrollTop() >= elementTop){
+        $('body').addClass('nav_fixed');
+    }else{
+        $('body').removeClass('nav_fixed');
+    }
+});
+
+$('.btn-menu').on('click', function(){
+    $('.nav').toggleClass('nav-toggle');
+})
+
+/*scripts*/
